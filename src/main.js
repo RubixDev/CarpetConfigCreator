e74bd6b5422ee4134eb7be49f47c792db425acfd
RubixDev <silas.groh@t-online.de>
2021-06-15 18:38:59 +0200

    Make categories update on mod change

--- src/main.js
+++ src/main.js
@@ -22,6 +22,89 @@ let defaultValues = {}
 
 let data = {}
 
+window.onload = async function () {
+    // Parse markdown rule lists
+    for (const [modId, mod] of modInfo.entries()) {
+        data[modId] = new Mod(modId, mod.name, {})
+        await fetch(mod.url)
+            .then(r => r.text())
+            .then(r => {
+                for (const rule of r.split(mod.splitString + ' ').slice(1)) {
+                    const parsedRule = Rule.fromMarkdown(rule)
+                    data[modId].rules[parsedRule.name] = parsedRule
+                }
+            })
+        data[modId].rules = data[modId].rules.sorted()
+    }
+    print(data)
+    for (const mod of data.values()) {
+        for (const rule of mod.rules.values()) {
+            defaultValues[rule.name] = rule.value
+        }
+    }
+    defaultValues = defaultValues.sorted()
+    print(defaultValues)
+
+    // Create radio buttons for the mods
+    const radioButtons = document.getElementById('radioButtons')
+    const modButtonDiv = radioButtons.firstElementChild
+
+    for (let modIndex = 0; modIndex < data.keys().length; modIndex++) {
+        const mod = data[data.keys()[modIndex]]
+
+        const input = document.createElement('input')
+        input.type = 'radio'
+        input.id = mod.id
+        input.name = 'mod'
+        if (modIndex === 0) input.setAttribute('checked', '')
+        modButtonDiv.appendChild(input)
+
+        const label = document.createElement('label')
+        label.htmlFor = mod.id
+        label.innerText = mod.name
+        modButtonDiv.appendChild(label)
+    }
+
+    addRadioChangeListener(document.getElementsByName('mod'), updateCategories)
+
+    // Create radio buttons for the categories
+    updateCategories()
+}
+
+function updateCategories() {
+    const categoryButtonDiv = document.getElementById('radioButtons').lastElementChild
+    categoryButtonDiv.innerHTML = ''
+
+    const modCategories = getSelectedMod().getCategories()
+
+    const allCategoriesInput = document.createElement('input')
+    allCategoriesInput.type = 'radio'
+    allCategoriesInput.id = 'all'
+    allCategoriesInput.name = 'category'
+    allCategoriesInput.setAttribute('checked', '')
+    categoryButtonDiv.appendChild(allCategoriesInput)
+
+    const allCategoriesLabel = document.createElement('label')
+    allCategoriesLabel.htmlFor = 'all'
+    allCategoriesLabel.innerText = 'All'
+    categoryButtonDiv.appendChild(allCategoriesLabel)
+
+    for (let categoryIndex = 0; categoryIndex < modCategories.length; categoryIndex++) {
+        const category = modCategories[categoryIndex]
+
+        const input = document.createElement('input')
+        input.type = 'radio'
+        input.id = category.toLowerCase()
+        input.name = 'category'
+        categoryButtonDiv.appendChild(input)
+
+        const label = document.createElement('label')
+        label.htmlFor = category.toLowerCase()
+        label.innerText = category
+        categoryButtonDiv.appendChild(label)
+    }
+}
+
 class Mod {
     constructor(id, name, rules) {
         this.id = id
@@ -101,78 +184,11 @@ function getSelectedMod() {
     return false
 }
 
-window.onload = async function () {
-    // Parse markdown rule lists
-    for (const [modId, mod] of modInfo.entries()) {
-        data[modId] = new Mod(modId, mod.name, {})
-        await fetch(mod.url)
-            .then(r => r.text())
-            .then(r => {
-                for (const rule of r.split(mod.splitString + ' ').slice(1)) {
-                    const parsedRule = Rule.fromMarkdown(rule)
-                    data[modId].rules[parsedRule.name] = parsedRule
-                }
-            })
-        data[modId].rules = data[modId].rules.sorted()
-    }
-    print(data)
-    for (const mod of data.values()) {
-        for (const rule of mod.rules.values()) {
-            defaultValues[rule.name] = rule.value
-        }
-    }
-    defaultValues = defaultValues.sorted()
-    print(defaultValues)
-
-    // Create radio buttons for the mods
-    const radioButtons = document.getElementById('radioButtons')
-    const modButtonDiv = radioButtons.firstElementChild
-    const categoryButtonDiv = radioButtons.lastElementChild
-
-    for (let modIndex = 0; modIndex < data.keys().length; modIndex++) {
-        const mod = data[data.keys()[modIndex]]
-
-        const input = document.createElement('input')
-        input.type = 'radio'
-        input.id = mod.id
-        input.name = 'mod'
-        if (modIndex === 0) input.setAttribute('checked', '')
-        modButtonDiv.appendChild(input)
-
-        const label = document.createElement('label')
-        label.htmlFor = mod.id
-        label.innerText = mod.name
-        modButtonDiv.appendChild(label)
-    }
-
-    // Create radio buttons for the categories
-    const modCategories = getSelectedMod().getCategories()
-
-    const allCategoriesInput = document.createElement('input')
-    allCategoriesInput.type = 'radio'
-    allCategoriesInput.id = 'all'
-    allCategoriesInput.name = 'category'
-    allCategoriesInput.setAttribute('checked', '')
-    categoryButtonDiv.appendChild(allCategoriesInput)
-
-    const allCategoriesLabel = document.createElement('label')
-    allCategoriesLabel.htmlFor = 'all'
-    allCategoriesLabel.innerText = 'All'
-    categoryButtonDiv.appendChild(allCategoriesLabel)
-
-    for (let categoryIndex = 0; categoryIndex < modCategories.length; categoryIndex++) {
-        const category = modCategories[categoryIndex]
-
-        const input = document.createElement('input')
-        input.type = 'radio'
-        input.id = category.toLowerCase()
-        input.name = 'category'
-        categoryButtonDiv.appendChild(input)
-
-        const label = document.createElement('label')
-        label.htmlFor = category.toLowerCase()
-        label.innerText = category
-        categoryButtonDiv.appendChild(label)
+function addRadioChangeListener(radioButtons, func) {
+    for (const radioButton of radioButtons) {
+        radioButton.addEventListener('click', function () {
+            func()
+        })
     }
 }
 
