459c8012be01d8b0f0bbb2f554d529d7a1100baa
RubixDev <silas.groh@t-online.de>
2021-06-15 20:10:26 +0200

    Split to multiple files

---
+++ src/classes.js
@@ -0,0 +1,71 @@
+class Mod {
+    constructor(id, name, rules) {
+        this.id = id
+        this.name = name
+        this.rules = rules
+    }
+
+    getCategories() {
+        let categories = []
+        for (const rule of this.rules.values()) {
+            for (const category of rule.categories) {
+                if (!categories.includes(category)) {
+                    categories.push(category)
+                }
+            }
+        }
+        return categories.sort()
+    }
+}
+
+class Rule {
+    constructor(type, name, value, options, isStrict, categories) {
+        this.type = type
+        this.name = name
+        this.value = value
+        this.options = options
+        this.isStrict = isStrict
+        this.categories = categories
+    }
+
+    static fromMarkdown(markdown) {
+        let type = markdown
+            .split('Type: ')
+            .last()
+            .split('\n')[0]
+            .replaceAll('`', '')
+            .replaceAll(' ', '')
+        let name = markdown
+            .split('\n')[0]
+        let value = markdown
+            .split('Default value: ')
+            .last()
+            .split('\n')[0]
+            .replaceAll('`', '')
+            .replaceAll(' ', '')
+        let options = markdown
+            .split('options: ')
+            .last()
+            .split('\n')[0]
+            .replaceAll('`', '')
+            .replaceAll(' ', '')
+            .split(',')
+        let strict = markdown
+            .split(' options')[0]
+            .split(' ')
+            .last()
+            .toLowerCase() === 'required'
+        let categories = markdown
+            .split('Categories: ')
+            .last()
+            .split('\n')[0]
+            .replaceAll('`', '')
+            .replaceAll(' ', '')
+            .toLowerCase()
+            .split(',')
+            .filter(category => !excludedCategories.includes(category))  // Filter out categories of whole mods
+            .map(category => category.charAt(0).toUpperCase() + category.slice(1))  // Capitalize every first letter
+
+        return new Rule(type, name, value, options, strict, categories)
+    }
+}
---
+++ src/utils.js
@@ -0,0 +1,52 @@
+// Prototypes
+Object.prototype.entries = function () {
+    return Object.entries(this)
+}
+
+Object.prototype.values = function () {
+    return Object.values(this)
+}
+
+Object.prototype.keys = function () {
+    return Object.keys(this)
+}
+
+Object.prototype.sorted = function () {
+    return this.keys().sort().reduce((obj, key) => {
+            obj[key] = this[key]
+            return obj
+        }, {})
+}
+
+Array.prototype.last = function () {
+    return this[this.length - 1]
+}
+
+// General
+function print(...data) {
+    console.log(...data)
+}
+
+function addRadioChangeListener(radioButtons, func) {
+    for (const radioButton of radioButtons) {
+        radioButton.addEventListener('click', function () {
+            func()
+        })
+    }
+}
+
+// Website specific
+function getSelectedMod() {
+    for (const modId of data.keys()) {
+        if (document.getElementById(modId).checked) return data[modId]
+    }
+    return null
+}
+
+function getSelectedCategory() {
+    for (const category of getSelectedMod().getCategories()) {
+        if (document.getElementById(category.toLowerCase()).checked) return category
+    }
+    if (document.getElementById('all').checked) return 'all'
+    return null
+}
